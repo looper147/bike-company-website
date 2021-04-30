3023233cdebc83e23d081d15fb0e9e53f1a1bd4e
looper <hadismsmanii@gmail.com>
2021-04-30 16:00:23 +0300

    nm

--- src/components/navigationBar/navigation.bar.js
+++ src/components/navigationBar/navigation.bar.js
@@ -4,47 +4,76 @@ import LightMode from "./components/lightMode.js";
 import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
 export default function NavigationBar() {
     return (
-        <div className="navbar">
-            <button className="navBt">Home</button>
-            <button className="navBt">News</button>
-            <div className="dropdown">
-                <button className="dropbtn">Types<ArrowDropDownIcon /></button>
-                <div className="dropdown-content">
-                    <button>Street</button>
-                    <button>Off-road</button>
-                    <button>Dual purpose</button>
-                </div>
-            </div>
-            <div className="dropdown">
-                <button className="dropbtn">Model Year<ArrowDropDownIcon /></button>
-                <div className="dropdown-content">
-                    <button>1970-2000</button>
-                    <button>2001-2010</button>
-                    <button>2010-2021</button>
-                </div>
-            </div>
-            <div className="dropdown">
-                <button className="dropbtn">Manufacturers<ArrowDropDownIcon /></button>
-                <div className="dropdown-content">
-                    <button>Harley-Davidson	</button>
-                    <button>Honda</button>
-                    <button>Yamaha</button>
-                    <button>Kawasaki</button>
-                    <button>Suzuki</button>
-                </div>
-            </div>
-            <SearchBox />
-            <div style={{ float: "right" }}>
-                {/*light mode*/}
-                <label>
-                    Light Mode
-                    <LightMode style={{ display: "inline-block" }} />
-                </label>
+        <>
+            <nav>
+
+                <ul>
+                    <li>
+                        <button className="navBt">Home<ArrowDropDownIcon /></button>
+                    </li>
+
+                    <li>
+                        <button className="navBt">News<ArrowDropDownIcon /></button>
+                    </li>
+
+                    <li>
+                        <div className="dropdown">
+                            <button className="dropbtn">Types<ArrowDropDownIcon /></button>
+                            <div className="dropdown-content">
+                                <button>Street</button>
+                                <button>Off-road</button>
+                                <button>Dual purpose</button>
+                            </div>
+                        </div>
+                    </li>
+
+                    <li>
+                        <div className="dropdown">
+                            <button className="dropbtn">Model Year<ArrowDropDownIcon /></button>
+                            <div className="dropdown-content">
+                                <button>1970-2000</button>
+                                <button>2001-2010</button>
+                                <button>2010-2021</button>
+                            </div>
+                        </div>
+                    </li>
+
+                    <li>
+                        <div className="dropdown">
+                            <button className="dropbtn">Manufacturers<ArrowDropDownIcon /></button>
+                            <div className="dropdown-content">
+                                <button>Harley-Davidson	</button>
+                                <button>Honda</button>
+                                <button>Yamaha</button>
+                                <button>Kawasaki</button>
+                                <button>Suzuki</button>
+                            </div>
+                        </div>
+                    </li>
+
+                    <li>
+                        <SearchBox style={{ border: "none" }} />
+                    </li>
+
+                    <li style={{ float: "right", width: "20%" }}>
+                        {/*light mode*/}
+                        <label>
+                            Light Mode
+                        <LightMode style={{ display: "inline-block" }} />
+                        </label>
 
 
-            </div>
+                    </li>
+                </ul>
 
 
-        </div>
+
+
+
+
+            </nav>
+
+        </>
+
     )
 }
