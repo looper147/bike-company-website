06e4df82db58b7648267d75ff0609cdcd435817b
looper <hadismsmanii@gmail.com>
2021-05-03 01:11:09 +0300

    nm

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,5 +1,5 @@
 import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
+// import { makeStyles } from "@material-ui/core/styles";
 
 // const styles = makeStyles({
 //     text: {
